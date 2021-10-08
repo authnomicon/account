47016e3e51f9ba1005073198c1b53858138e48e3
Jared Hanson <jaredhanson@gmail.com>
2021-10-07 18:33:59 -0700

    Remove ceremony from signup and move to prompt.

--- app/signup/http/handlers/prompt.js
+++ app/signup/http/handlers/prompt.js
@@ -1,8 +1,33 @@
-exports = module.exports = function(ceremony) {
+exports = module.exports = function(csrfProtection, state) {
+  var path = require('path')
+    , ejs = require('ejs');
+
+
+  function render(req, res, next) {
+    res.render('account/signup', function(err, str) {
+      if (err && err.view) {
+        var view = path.resolve(__dirname, '../views/prompt.ejs');
+        ejs.renderFile(view, res.locals, function(err, str) {
+          if (err) { return next(err); }
+          res.send(str);
+        });
+        return;
+      } else if (err) {
+        return next(err);
+      }
+      res.send(str);
+    });
+  };
   
-  return ceremony('signup');
+  
+  return [
+    csrfProtection(),
+    state(),
+    prompt
+  ];
 };
 
 exports['@require'] = [
-  'http://i.bixbyjs.org/http/middleware/ceremony'
+  'http://i.bixbyjs.org/http/middleware/csrfProtection',
+  'http://i.bixbyjs.org/http/middleware/state'
 ];
--- app/signup/http/service.js
+++ app/signup/http/service.js
@@ -1,18 +1,15 @@
 exports = module.exports = function(promptHandler, enrollHandler) {
   var express = require('express');
-  var router = new express.Router();
   
+  var router = new express.Router();
   router.get('/', promptHandler);
   router.post('/', enrollHandler);
   
   return router;
 };
 
-exports['@implements'] = [
-  'http://i.bixbyjs.org/http/Service',
-  'http://schemas.authnomicon.org/js/signup/HTTPService'
-];
-exports['@path'] = '/signup';
+exports['@implements'] = 'http://i.bixbyjs.org/http/Service';
+exports['@path'] = '/account/create';
 exports['@require'] = [
   './handlers/prompt',
   './handlers/enroll'
