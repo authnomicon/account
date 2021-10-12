93f76afcf70c24aec0183cab0c899a127c32c923
Jared Hanson <jaredhanson@gmail.com>
2021-10-11 18:54:44 -0700

    Resume state after selecting account.

--- app/select/http/handlers/select.js
+++ app/select/http/handlers/select.js
@@ -1,11 +1,10 @@
 exports = module.exports = function(parse, csrfProtection, authenticate, state, session) {
   
   function select(req, res, next) {
-    console.log('select this account:');
-    console.log(req.body);
-    console.log(req.user);
+    var selector = req.body.session_selector;
     
-    return;
+    // TODO: Yield the selector forward.
+    return res.resumeState(next);
     
     // WIP: add multi-account login support to passport and session manager
   }
