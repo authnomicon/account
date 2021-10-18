183902340f28512752f044aad6f956d69bb13e9a
Jared Hanson <jaredhanson@gmail.com>
2021-10-18 09:00:32 -0700

    Stub out test.

--- test/signup/http/handlers/create.test.js
+++ test/signup/http/handlers/create.test.js
@@ -1,1 +1,11 @@
-// TODO
+/* global describe, it, expect */
+
+var expect = require('chai').expect;
+var chai = require('chai');
+var sinon = require('sinon');
+var factory = require('../../../../app/signup/http/handlers/create');
+
+
+describe('signup/http/handlers/create', function() {
+  
+});
