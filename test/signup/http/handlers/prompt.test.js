89ed5b14074f88414ef6cf07fde5592c3086e748
Jared Hanson <jaredhanson@gmail.com>
2021-10-18 08:46:39 -0700

    Clean up signup tests.

--- test/signup/http/handlers/prompt.test.js
+++ test/signup/http/handlers/prompt.test.js
@@ -1,66 +1,55 @@
 /* global describe, it, expect */
 
-var chai = require('chai');
 var expect = require('chai').expect;
+var chai = require('chai');
 var sinon = require('sinon');
-//var flowstate = require('flowstate');
 var factory = require('../../../../app/signup/http/handlers/prompt');
 
 
-describe.skip('signup/http/handlers/prompt', function() {
+describe('signup/http/handlers/prompt', function() {
   
-  it('should export factory function', function() {
-    expect(factory).to.be.a('function');
-  });
+  var handler;
   
-  it('should be annotated', function() {
-    expect(factory['@implements']).to.be.undefined;
-    expect(factory['@singleton']).to.be.undefined;
-  });
-  
-  describe('handler', function() {
-    /*
-    var manager = new flowstate.Manager();
-    manager.use('signup', {
-      prompt:  [
-        function(req, res, next) {
-          res.render('signup');
-        }
-      ]
-    });
-    */
-    
-    function ceremony(name) {
-      return manager.flow.apply(manager, arguments);
+  before(function() {
+    function csrfProtection() {
+      return function(req, res, next) {
+        req.csrfToken = function() {
+          return 'i8XNjC4b8KVok4uw5RftR38Wgp2BFwql';
+        };
+        next();
+      };
     }
     
+    function state() {
+      return function(req, res, next) {
+        next();
+      };
+    }
     
-    describe('prompting', function() {
-      var request, response, view;
-      
-      before(function(done) {
-        var handler = factory(ceremony);
-        
-        chai.express.handler(handler)
-          .req(function(req) {
-            request = req;
-          })
-          .res(function(res) {
-            response = res;
-          })
-          .render(function(res, v) {
-            view = v;
-            done();
-          })
-          .dispatch();
-      });
-      
-      it('should render', function() {
-        expect(response.statusCode).to.equal(200);
-        expect(view).to.equal('signup');
-      });
-    });
+    var csrfProtectionSpy;
+    var stateSpy;
     
+    csrfProtectionSpy = sinon.spy(csrfProtection);
+    stateSpy = sinon.spy(state);
+    
+    handler = factory(csrfProtectionSpy, stateSpy);
+    
+    expect(csrfProtectionSpy).to.be.calledOnce;
+    expect(stateSpy).to.be.calledOnce;
   });
+  
+  it('should prompt to create account', function(done) {
+    
+    chai.express.use(handler)
+      .finish(function() {
+        expect(this).to.have.status(200);
+        expect(this).to.render('account/signup').with.deep.locals({
+          csrfToken: 'i8XNjC4b8KVok4uw5RftR38Wgp2BFwql'
+        });
+        done();
+      })
+      .listen();
+    
+  }); // should prompt to create account
   
 });
